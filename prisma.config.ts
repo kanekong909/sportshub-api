eec1543246ae3c281c59e494524f48a40d3887cb
MateoBossa <mateobossa909@gmail.com>
2026-05-28 18:39:00 -0500

    correcion en dockerfile v3

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
   migrations: {
     seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
   },
